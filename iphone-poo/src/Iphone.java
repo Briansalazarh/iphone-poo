a64332aba3beda9bad723e051f2c8d723bec9f0d
Brian <128023953+Briansalazarh@users.noreply.github.com>
2026-01-12 00:18:11 -0300

    3

---
+++ iphone-poo/src/Iphone.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Iphone implements
+        ReprodutorMusical,
+        AparelhoTelefonico,
+        NavegadorInternet {
+
+    public void tocar() {}
+    public void pausar() {}
+    public void selecionarMusica(String musica) {}
+
+    public void ligar(String numero) {}
+    public void atender() {}
+    public void iniciarCorreioVoz() {}
+
+    public void exibirPagina(String url) {}
+    public void adicionarNovaAba() {}
+    public void atualizarPagina() {}
+}
---
+++ iphone-poo/src/ReprodutorMusical.java
@@ -0,0 +1,8 @@
+package src;
+
+public interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
+
